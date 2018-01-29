8173b90bc93b62b98c76632e0adc9b38ce0f13a7
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-29 04:37:06 -0600

    Sixth Commit. It's now 4:36 am. I tried to combine all the files to one string. However, I was concerned that the brackets will mess things up in the code? I am not even sure if they will mess up my code, but I was trying to removie them. Looking up and attempting to use regex.

--- src/CourseGraderData.java
+++ src/CourseGraderData.java
@@ -11,17 +11,36 @@ import java.util.List;
 
 
 public class CourseGraderData {
-    public static void main (String[] args) {
-        Gson aishik = new Gson();
-        Data.getJsonFilesAsList();
-        for (int i = 0; i < 6; i++) {
+    private static CourseProperties please;
+    Gson aishik = new Gson();
+    public static void main(String[] args) {
+    //public static String combine1() {
 
-            System.out.print(Data.getFileContentsAsString(Data.getJsonFilesAsList().get(i)));
-        }
+
+
+        String fall2013 = Data.getFileContentsAsString(Data.getJsonFilesAsList().get(0));
+        String fall2014 = Data.getFileContentsAsString(Data.getJsonFilesAsList().get(1));
+        String spring2013 = Data.getFileContentsAsString(Data.getJsonFilesAsList().get(2));
+        String spring2014 = Data.getFileContentsAsString(Data.getJsonFilesAsList().get(3));
+        String summer2013 = Data.getFileContentsAsString(Data.getJsonFilesAsList().get(4));
+        String summer2014 = Data.getFileContentsAsString(Data.getJsonFilesAsList().get(5));
+        String combine = fall2013 + fall2014 + spring2013 + spring2014 + summer2013 + summer2014;
+        combine =  combine.replaceAll("\\[", "").replaceAll("\\]","");
+
+        System.out.print(combine);
+
+
+
+
+
+
+        //for (int i = 0; i < Data.JSON_FILES.length; i++) {
+
+            //System.out.print(Data.getFileContentsAsString(Data.getJsonFilesAsList().get(i)));
+        //}
 
 
     }
-
 
 
 }
