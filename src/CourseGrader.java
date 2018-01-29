8f7df8170a2cafe04c435edbfe6569a4dd616357
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-28 23:02:26 -0600

    Third Commit. I made finished making the Getter class,with all the get Methods. Imported GSON library. I was really struggling to make it pass the test cases. The mistake turned out to be that I forgot the capitals, like the String had to match exactly.

--- src/CourseGrader.java
+++ src/CourseGrader.java
@@ -1,2 +1,59 @@
 public class CourseGrader {
+
+
+    private String Subject;
+    private String Title;
+    private String Section;
+    private String Type;
+    private String Instructor;
+    private int CRN;
+    private int Term;
+    private int Number;
+    private int[] Grades;
+    private double Average;
+
+    public CourseGrader(String subject) {
+
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getSection() {
+        return Section;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public String getInstructor() {
+        return Instructor;
+    }
+
+    public int getCRN() {
+        return CRN;
+    }
+
+    public int getTerm() {
+        return Term;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public int[] getGrades() {
+        return Grades;
+    }
+
+    public double getAverage() {
+        return Average;
+    }
 }
+
--- test/CourseGraderTest.java
+++ test/CourseGraderTest.java
@@ -1,5 +1,85 @@
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CourseGraderTest {
 
+    private static final String TESTER_JSON = "{\n" +
+            "   \"CRN\":41758,\n" +
+            "   \"Subject\":\"AAS\",\n" +
+            "   \"Number\":100,\n" +
+            "   \"Title\":\"Intro Asian American Studies\",\n" +
+            "   \"Section\":\"AD1\",\n" +
+            "   \"Type\":\"DIS\",\n" +
+            "   \"Term\":120138,\n" +
+            "   \"Instructor\":\"Arai, Sayuri\",\n" +
+            "   \"Grades\":[  \n" +
+            "      6,\n" +
+            "      16,\n" +
+            "      5,\n" +
+            "      3,\n" +
+            "      2,\n" +
+            "      3,\n" +
+            "      0,\n" +
+            "      0,\n" +
+            "      0,\n" +
+            "      0,\n" +
+            "      0,\n" +
+            "      0,\n" +
+            "      0,\n" +
+            "      0\n" +
+            "   ],\n" +
+            "   \"Average\":3.72\n" +
+            "   }";
+
+
+    private static CourseGrader courseGrader;
+    @Before
+    public void setUp() throws Exception {
+        Gson gson = new Gson();
+        courseGrader = gson.fromJson(TESTER_JSON, CourseGrader.class);
+    }
+
+    @Test
+    public void getSubject() {
+
+        assertEquals("AAS", courseGrader.getSubject());
+    }
+    @Test
+    public void getCRN() {
+
+    }
+    @Test
+    public void getCourseNumber() {
+    }
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void getSection() {
+    }
+
+    @Test
+    public void getType() {
+    }
+
+    @Test
+    public void getInstructor() {
+    }
+
+    @Test
+    public void getTerm() {
+    }
+
+    @Test
+    public void getGrades() {
+    }
+
+    @Test
+    public void getAverage() {
+    }
 }
